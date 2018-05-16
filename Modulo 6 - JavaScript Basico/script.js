cd343070d82eddd2d24a651c06d1342933074cad
Athos Francisco <afpc@a.recife.ifpe.edu.br>
2018-05-16 18:40:02 -0300

    JavaScript Básico

--- Modulo 6 - JavaScript Basico/script.js
+++ Modulo 6 - JavaScript Basico/script.js
@@ -3,4 +3,32 @@
 	Isto é outro comentário
 	
 */
-var nome = "Athos";
+var nome;
+
+window.onload = function(){
+	//nome = prompt("Qual seu nome?");
+
+	//document.write(nome);
+};
+
+//https://jsfiddle.net/
+
+function fazerAcao(nome){
+	var area = document.getElementById("area");
+	var texto = prompt("Qual seu sobrenome?");
+
+	area.innerHTML = nome +" "+ texto;
+}
+
+function adicionarIngrediente(){
+	var ing = document.getElementById("ingrediente").value;
+	var lista = document.getElementById("lista").innerHTML;
+
+	lista = lista + "<li>"+ing+"</li>";
+
+	document.getElementById("lista").innerHTML = lista;
+}
+
+
+
+var x = 10+20;
