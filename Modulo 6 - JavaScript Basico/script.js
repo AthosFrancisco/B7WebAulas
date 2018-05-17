baff38a731b0e0ac23d2909fd5f5bcd5215523a8
AthosFrancisco <afpc@a.recife.ifpe.edu.br>
2018-05-17 12:29:46 -0300

    JavaScript Básico

--- Modulo 6 - JavaScript Basico/script.js
+++ Modulo 6 - JavaScript Basico/script.js
@@ -29,6 +29,15 @@ function adicionarIngrediente(){
 	document.getElementById("lista").innerHTML = lista;
 }
 
-
-
 var x = 10+20;
+
+//===============================================
+
+function somar(){
+	var campo1 = parseInt(document.getElementById("campo1").value);
+	var campo2 = parseInt(document.getElementById("campo2").value);
+
+	var soma = campo1+campo2;
+
+	alert(soma);
+}
