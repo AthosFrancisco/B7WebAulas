18a2123315e5f6ddd20acdc8f6162b28f83654a3
AthosFrancisco <afpc@a.recife.ifpe.edu.br>
2018-05-18 12:04:54 -0300

    JavaScript Básico

--- Modulo 6 - JavaScript Basico/script2.js
+++ Modulo 6 - JavaScript Basico/script2.js
@@ -35,3 +35,24 @@ switch(x){
 		alert("x é indefinido");
 		break;
 }*/
+
+window.onload = function(){
+	var n1 = document.getElementById("n1");
+	n1.innerHTML = Math.floor(Math.random()*10);
+	n1.style.display = "none";
+};
+
+function comparar(){
+
+	var n = document.getElementById("n1");
+	var n1 = parseInt(n.innerHTML);
+	var n2 = parseInt(document.getElementById("n2").value);
+
+	n.style.display = "block";
+
+	if (n1 == n2) {
+		alert(n1+" e "+n2+" são iguais");
+	}else{
+		alert(n1+" e "+n2+" não são iguais");
+	}
+}
