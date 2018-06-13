7caf0aba9b1c1701122ae0be6551e126a1c85d39
Athos Francisco <afpc@a.recife.ifpe.edu.br>
2018-06-13 15:59:53 -0300

    demais aulas de javascript intermediário

--- Modulo 7 - JavaScript Intermediário/script.js
+++ Modulo 7 - JavaScript Intermediário/script.js
@@ -31,3 +31,62 @@ function botaoDireito(){
 function cliqueDuplo(){
 	console.log("Clicou duas vezes");
 }
+
+//eventos de teclado
+//ver demais propriedades no mdn
+function apertouBotao(event){
+	console.log("Apertou a tecla: "+ event.keyCode);
+}
+
+function soltouBotao(){
+	console.log("Soltou a tecla: "+ event.keyCode);
+}
+
+function pressionouBotao(){
+	console.log("Pressionou a tecla: "+ event.keyCode);
+}
+
+//eventos de página
+
+function carregou(){
+	alert("carregou");
+}
+
+//eventos de formulários
+
+function mudouOpcao(obj){
+	console.log("selecionou: "+obj.value);
+}
+
+function focar(){
+	console.log("focou");
+}
+
+function desfocar(){
+	console.log("desfocou");
+}
+
+//jogo das bolinnhas
+
+
+
+function addBola(){
+	var bola = document.createElement("div");
+	bola.setAttribute("class", "bola");
+
+	var p1 = Math.floor(Math.random()*500);
+	var p2 = Math.floor(Math.random()*400);
+
+	bola.setAttribute("style", "left:"+p1+"px; top:"+p2+"px;");
+	bola.setAttribute("onclick", "estourar(this)");
+
+	document.body.appendChild(bola);
+}
+
+function estourar(obj){
+	document.body.removeChild(obj);
+}
+
+function iniciar(){
+	setInterval(addBola, 1000);
+}
